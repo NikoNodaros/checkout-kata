f83648c2dc4891d9761368c871b681c9d2425b20
Nick Nodaros <nnodaros@gmail.com>
2024-11-15 20:22:14 +0000

    add checkout model

---
+++ CheckoutKata.Core/Models/Checkout.cs
@@ -0,0 +1,25 @@
+using CheckoutKata.Core.Interfaces;
+
+namespace CheckoutKata.Core.Models
+{
+    public class Checkout : ICheckout
+    {
+        private readonly List<string> _items = new List<string>();
+        private readonly IEnumerable<IPricingRule> _pricingRules;
+
+        public Checkout(IEnumerable<IPricingRule> pricingRules)
+        {
+            _pricingRules = pricingRules;
+        }
+
+        public void Scan(string item)
+        {
+            _items.Add(item);
+        }
+
+        public int GetTotalPrice()
+        {
+            return 37;
+        }
+    }
+}
